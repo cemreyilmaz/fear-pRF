e068c53bfd88074093dedd03e7121e23d74ec218
Cemre Yılmaz <cemreyilmaz@yahoo.com>
2022-07-21 22:15:27 +0300

    measure of effect-size

--- stats/pRFestimatesRANOVA.m
+++ stats/pRFestimatesRANOVA.m
@@ -12,6 +12,10 @@ EstFolder = '/Users/cemreyilmaz/Documents/fear-pRF/data/estimates';
 cd(EstFolder)
 var = 'Hemispheres'; % post-hoc variable: ROI / Hemispheres
 % ----------------------------------------------------------------------- %
+% effect size toolbox
+% https://www.mathworks.com/matlabcentral/fileexchange/32398-hhentschke-measures-of-effect-size-toolbox?w.mathworks.com
+addpath(genpath('/Users/cemreyilmaz/Documents/fear-pRF/Codes/statAnalysis/hhentschke_measures-of-effect-size-toolbox'))
+% ----------------------------------------------------------------------- %
 % pRFcenter location 
 Data = readtable('pRFcenter.csv');
 within = table([1 2 3]','VariableNames',{'Conditions'});
@@ -20,7 +24,7 @@ rm_pRF = fitrm(Data,'Scrambled-Emotional~Subjects+Hemispheres*ROI',...
 ranovatbl_pRF = ranova(rm_pRF);
 tbl_pRF = multcompare(rm_pRF,var,'Alpha', 0.05, ...
     'ComparisonType', 'bonferroni');
-
+stats_pRF = mes1way([Data.Scrambled,Data.Neutral,Data.Emotional],'eta2');
 % Value, e.g. Sigma
 Data = readtable([Val '.csv']);
 within = table([1 2 3]','VariableNames',{'Conditions'});
@@ -29,19 +33,29 @@ rm_val = fitrm(Data,'Scrambled-Emotional~Subjects+Hemispheres*ROI',...
 ranovatbl_val = ranova(rm_val);
 tbl_val = multcompare(rm_val,var,'Alpha', 0.05, ...
     'ComparisonType', 'bonferroni');
+stats_val = mes1way([Data.Scrambled,Data.Neutral,Data.Emotional],'eta2');
 % ----------------------------------------------------------------------- %
 % Save results for rmANOVA
 % ranavotbl cannot be saved as it is. So, I rearranged it.
+p = table({'isDep';'nBoot';'confLevel';'eta2';'eta2Ci-min';'eta2Ci-max'}, ...
+    [stats_pRF.isDep;stats_pRF.nBoot;stats_pRF.confLevel;stats_pRF.eta2;...
+    stats_pRF.eta2Ci(1);stats_pRF.eta2Ci(2)]);
 ranovatbl_pRF = table(ranovatbl_pRF);
 ranovatbl_pRF=[ranovatbl_pRF.Properties.RowNames,
     ranovatbl_pRF.ranovatbl_pRF(1,:);ranovatbl_pRF.ranovatbl_pRF(2,:);...
     ranovatbl_pRF.ranovatbl_pRF(3,:);ranovatbl_pRF.ranovatbl_pRF(4,:);...
     ranovatbl_pRF.ranovatbl_pRF(5,:);ranovatbl_pRF.ranovatbl_pRF(6,:)];
+ranovatbl_pRF = [ranovatbl_pRF,p];
+% ----------------------------------------------------------------------- %
+v = table({'isDep';'nBoot';'confLevel';'eta2';'eta2Ci-min';'eta2Ci-max'}, ...
+    [stats_val.isDep;stats_val.nBoot;stats_val.confLevel;stats_val.eta2;...
+    stats_val.eta2Ci(1);stats_val.eta2Ci(2)]);
 ranovatbl_val = table(ranovatbl_val);
 ranovatbl_val=[ranovatbl_val.Properties.RowNames,
     ranovatbl_val.ranovatbl_val(1,:);ranovatbl_val.ranovatbl_val(2,:);...
     ranovatbl_val.ranovatbl_val(3,:);ranovatbl_val.ranovatbl_val(4,:);...
     ranovatbl_val.ranovatbl_val(5,:);ranovatbl_val.ranovatbl_val(6,:)];
+ranovatbl_val = [ranovatbl_val,v];
 cd(StatFolder)
 writetable(ranovatbl_pRF, 'rmANOVA_pRFcenter.csv','WriteRowNames',true)
 writetable(ranovatbl_val, ['rmANOVA_' Val '.csv'],'WriteRowNames',true)
